44fff7e9b829233aea829d05d7cdb7889adff71b
AlexanderDispenza <alexanderdispenza@gmail.com>
2019-02-07 01:41:39 -0500

    Added Particles
    NOTE: Particles can not currently be seen due to the defualt material on the sprites .

--- SpiritRunnerProject/Assets/Scripts/PlayerController.cs
+++ SpiritRunnerProject/Assets/Scripts/PlayerController.cs
@@ -8,23 +8,33 @@ public class PlayerController : MonoBehaviour {
 
     Rigidbody2D rb2d;
 
+    public int pointValue;
+
 	// Use this for initialization
 	void Start ()
     {
         speed = 3.0f;
-
+        pointValue = 0;
         rb2d = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
-
         float moveVertical = Input.GetAxisRaw("Vertical");
-
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
         rb2d.AddForce(movement * speed);
-	}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            pointValue++;
+            Debug.Log("Points: " + pointValue);
+            Destroy(collision.gameObject);
+        }
+    }
+
 }
