dff5c83bc534c71570ed51077025c89555f5dc13
AlexanderDispenza <alexanderdispenza@gmail.com>
2019-02-06 21:32:11 -0500

    Added Light Source
    Light follows player and casts to near by objects

---
+++ SpiritRunnerProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start ()
+    {
+        speed = 3.0f;
+
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(movement * speed);
+	}
+}
